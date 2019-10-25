478693ea3da351e188cc1eeb5f183d6d3667a9f2
kubavation <kubavation@wp.pl>
2019-10-25 15:38:30 +0200

    location interface

---
+++ restaurant-finder/src/app/map/model/Location.ts
@@ -0,0 +1,15 @@
+import { Marker } from './Marker';
+
+export interface Location {
+    lat: number;
+    lng: number;
+    viewport?: object;
+    zoom: number;
+    address_level_1?: string;
+    address_level_2?: string;
+    address_country?: string;
+    address_zip?: string;
+    address_state?: string;
+    marker?: Marker;
+  }
+  
