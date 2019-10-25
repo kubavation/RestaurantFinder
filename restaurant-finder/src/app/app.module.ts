2e169bd967b4e2021a4a7db294f7acffc338122c
kubavation <kubavation@wp.pl>
2019-10-25 15:31:20 +0200

    imports in app module

--- restaurant-finder/src/app/app.module.ts
+++ restaurant-finder/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    AgmCoreModule,
+    GoogleMapsAPIWrapper
   ],
   providers: [],
   bootstrap: [AppComponent]
