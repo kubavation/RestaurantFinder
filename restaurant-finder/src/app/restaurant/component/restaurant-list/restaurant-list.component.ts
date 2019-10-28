53d5c7188bad9f5888d81a2ebaf68a986e536a53
kubavation <kubavation@wp.pl>
2019-10-28 17:29:12 +0100

    added async pipe in restaurant list component

--- restaurant-finder/src/app/restaurant/component/restaurant-list/restaurant-list.component.ts
+++ restaurant-finder/src/app/restaurant/component/restaurant-list/restaurant-list.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { State } from 'src/app/reducer/map.reducer';
 import { getChosenLocation } from 'src/app/selector/map.selector';
+import { filter, map, tap, flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-restaurant-list',
@@ -12,19 +13,18 @@ import { getChosenLocation } from 'src/app/selector/map.selector';
 })
 export class RestaurantListComponent implements OnInit {
 
-  private test$: Observable<any>;
-  private test2: Array<any> = [];
+  private places$: Observable<any>;
 
   constructor(private restaurantService: RestaurantService,
               private store: Store<State>) { }
 
   ngOnInit() {
-    // this.restaurantService.findByLocation({lat: 51.0898838, lng: 17.0372})
-    //   .subscribe(r => {console.log(r); this.test = r} );
-    this.store.pipe(select(getChosenLocation))
-        .subscribe(l => { console.log('hereee ', l); if(l != null) this.restaurantService.findByLocation(l)
-            .subscribe( r =>  { console.log(r); this.test2 = r } ); } );
-    
+    this.places$ =  this.store.pipe(
+        select(getChosenLocation),
+        filter(res => res),
+        flatMap(r => this.restaurantService.findByLocation(r)),
+        tap(r => console.log(r))
+      );
   }
 
 
--- restaurant-finder/src/app/restaurant/service/restaurant.service.ts
+++ restaurant-finder/src/app/restaurant/service/restaurant.service.ts
@@ -12,8 +12,8 @@ export class RestaurantService {
 
   constructor(private http: HttpClient) { }
 
-
   findByLocation({lat, lng}) {
+    console.log('????????/')
     return this.http.get<any>(this.basicUrl + `?count=10&lat=${lat}&lon=${lng}`, //todo radius?
      {headers: this.buildHttpHeaders() });
   }
@@ -24,6 +24,7 @@ export class RestaurantService {
       'user-key': environment.zomatoKey
     });
   }
+
 
 
 }
