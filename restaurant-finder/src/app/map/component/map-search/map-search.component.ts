cb4f2338e06b2f50503ba4f5983f0bd56c467293
kubavation <kubavation@wp.pl>
2019-10-28 09:18:04 +0100

    dispatching action on place changed

--- restaurant-finder/src/app/map/component/map-search/map-search.component.ts
+++ restaurant-finder/src/app/map/component/map-search/map-search.component.ts
@@ -21,11 +21,10 @@ export class MapSearchComponent implements OnInit {
 
   ngOnInit() {
     this.init();
-    this.store.dispatch(findLocation({lat: 2.32, lng: 3.21})); 
-    console.log('dispatched')
   }
 
   init() {
+
     this.mapsApiLoader.load().then(() => {
 
       const autocomplete = new google.maps.places.Autocomplete(
@@ -33,19 +32,24 @@ export class MapSearchComponent implements OnInit {
           types: ['address']
       });
 
-      autocomplete.addListener('place_changed', () => {
-        this.zone.run(() => {
+      autocomplete.addListener('place_changed', () => this.onPlaceChanged(autocomplete));
+  });
 
-          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+}
 
-          if (place.geometry == null) {
-            return;
-          }
+onPlaceChanged(element) {
+  this.zone.run(() => {
+    console.log('on place changed')
+    const place: google.maps.places.PlaceResult = element.getPlace();
 
-          // this.location.lat = place.geometry.location.lat();
-          // this.location.lng = place.geometry.location.lng();
-        });
-      });
+    if (place.geometry == null) {
+      return;
+    }
+
+    this.store.dispatch(findLocation({
+        lat: place.geometry.location.lat(),
+        lng: place.geometry.location.lng()
+      }));
   });
 }
 
