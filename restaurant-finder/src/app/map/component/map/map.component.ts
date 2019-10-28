d2094f14e0bd25f22e3087053e0ce2ad1b46c530
kubavation <kubavation@wp.pl>
2019-10-28 13:29:23 +0100

    working refreshing map

--- restaurant-finder/src/app/map/component/map/map.component.ts
+++ restaurant-finder/src/app/map/component/map/map.component.ts
@@ -5,6 +5,7 @@ import { AgmMap, MapsAPILoader, GoogleMapsAPIWrapper } from '@agm/core';
 import { Store, select } from '@ngrx/store';
 import { State } from 'src/app/reducer/map.reducer';
 import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
 
 declare const google: any;
 
@@ -20,6 +21,8 @@ export class MapComponent implements OnInit {
   @ViewChild(AgmMap, {static: false})
   private map: AgmMap;
 
+  public lat;
+  public lng;
   //for testing 
   public location: Location = {
     lat: 51.678418,
@@ -51,7 +54,17 @@ export class MapComponent implements OnInit {
   ngOnInit() {
     this.location.marker.draggable = true;
     //this.location$ = this.store.pipe(select(getChosenLocation));
-    this.location$ = this.store.select(getChosenLocation);
+    this.location$ = this.store.pipe(select(getChosenLocation)); 
+    // this.store.pipe(
+    //     select(getChosenLocation),
+    //     tap(r => console.log('in tap ', r))
+    //     );
+    //     //.subscribe(r => {console.log('hehe',r); this.location = this.setLocation(r)});
+
+    // this.store.pipe(
+    //   select(getChosenLocation),
+    //   tap(r => console.log('333', r))
+    // ).subscribe(w => { console.log(w); if(w != null) { this.lat = w.lat; this.lng = w.lng;} } )
 }
 
 
@@ -68,10 +81,28 @@ export class MapComponent implements OnInit {
             lng: position.coords.longitude,
             draggable: true
           }
-        };
+        };    
 
       });
     }
   }
+
+  setLocation(s) {
+    console.log('in set location')
+    console.log('here222', s)
+      return {
+        lat: s.lat,
+        lng: s.lng,
+        marker: {
+          lat: s.lat,
+          lng: s.lng,
+          draggable: true
+        }
+      };
+
+
+      // console.log('location: ')
+      // console.log(this.location)
+    };
 
 }
--- restaurant-finder/src/app/selector/map.selector.ts
+++ restaurant-finder/src/app/selector/map.selector.ts
@@ -3,11 +3,10 @@ import { State } from '../reducer/map.reducer';
 import { createSelector } from '@ngrx/store';
 
 
-//todo selector
 export const getLocationState = (state: any) => state.maps;
 //export const getChosenLocation = (state: State) => { console.log(state); return state};
 
 export const getChosenLocation = createSelector(
     getLocationState,
-    (state: any) => {console.log(state); return state.location;}
+    (state: any) => {console.log('?',state); return state.location;}
 );
